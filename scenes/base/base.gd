0b46ebd01db5b99c5930119b1acdb4a1702f0cc4
Elle Brooks <hello@ellebell.dev>
2025-10-29 07:06:31 -0700

    base scene
    - base scene at the end of the path
    - when enemy reaches the end of the path, they deals a set number of damage and get removed from the scene.
    - Currently, base take_damage just prints a statement to the console

---
+++ scenes/base/base.gd
@@ -0,0 +1,6 @@
+class_name PlayerBase
+
+extends Node3D
+
+func take_damage (damage: int) -> void:
+	print("base damaged by " + str(damage) + " point.")
--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -1,5 +1,10 @@
+class_name Enemy
+
 extends PathFollow3D
 @export var speed: float = 5
+@export var damage: int = 1
+
+@onready var base: PlayerBase = get_tree().get_first_node_in_group("base")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +14,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	progress += delta * speed
+	if (progress_ratio == 1):
+		base.take_damage(damage)
+		queue_free()
 	pass
